2a5a1ebc07f12d253854c130e6da7d506800d263
mattdrinksglue <myounger009@gmail.com>
2022-05-26 14:16:59 +0100

    Finish 09 and 10 exercises

--- javascript_exercises/09_palindromes/palindromes.js
+++ javascript_exercises/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let str = string.toLowerCase().replace(/[^\w\s\']| |_/g, "");
+    console.log(str);
+    let rev_string = str.split("").reverse().join("");
+    return (str == rev_string);
 };
 
 // Do not edit below this line
