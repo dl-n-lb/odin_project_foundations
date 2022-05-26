1e6a2e57195df9c9717af8c3d8cf73d9bc414f86
mattdrinksglue <myounger009@gmail.com>
2022-05-26 14:02:40 +0100

    Finish calculator exercise

--- javascript_exercises/08_calculator/calculator.js
+++ javascript_exercises/08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((xp, xn) => xp + xn, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.length ? args.reduce((xp, xn) => xp*xn) : 0;
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  return Math.pow(n, p);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+  for (let i = 1; i < n; i++) {
+    res *= i+1;
+  }
+  return res;
 };
 
 // Do not edit below this line
