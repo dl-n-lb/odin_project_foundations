d93ae5c57a82de282285dec450ea6a46fe1414ef
mattdrinksglue <myounger009@gmail.com>
2022-05-26 14:46:30 +0100

    finish 11 and 12 of js exercises

--- javascript_exercises/12_findTheOldest/findTheOldest.js
+++ javascript_exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest_prev, curr) => {
+        let prev_age = ("yearOfDeath" in oldest_prev) ? oldest_prev.yearOfDeath - oldest_prev.yearOfBirth : new Date().getFullYear() - oldest_prev.yearOfBirth;
+        let curr_age = ("yearOfDeath" in curr) ? curr.yearOfDeath - curr.yearOfBirth : Date.getFullYear() - curr.yearOfBirth;
+        return (curr_age > prev_age) ? curr : oldest_prev;
+    });
 };
 
 // Do not edit below this line
